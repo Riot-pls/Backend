fc5e20297516a60d512e7112205193a88e48b6c0
Julián Andrade <julian19896@gmail.com>
2019-08-06 12:11:27 -0500

    articulo, anteproyecto y proyecto

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 // Requires 
 
 var express = require('express');
-var mongoose= require('mongoose');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 //Inicializar variables
 
-var app=express();
+var app = express();
 
 //CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods","POST, GET ,PUT ,DELETE, OPTIONS");
-    
+    res.header("Access-Control-Allow-Methods", "POST, GET ,PUT ,DELETE, OPTIONS");
+
     next();
-  });
+});
 
 
 //Body Parser
@@ -26,30 +26,33 @@ app.use(bodyParser.json())
 
 //Importar Rutas
 
-var appRoutes= require('./routes/app');
+var appRoutes = require('./routes/app');
 
-var sedeUniversitaria= require('./routes/sedeUniversitaria');
-var programaUniversitario= require('./routes/programaUniversitario');
-var facultad= require('./routes/facultad');
+var sedeUniversitaria = require('./routes/sedeUniversitaria');
+var programaUniversitario = require('./routes/programaUniversitario');
+var facultad = require('./routes/facultad');
+var anteProyecto = require('./routes/anteProyecto');
+var proyecto = require('./routes/proyecto');
+var articulo = require('./routes/articulo');
 
-var usuarioRoutes= require('./routes/usuario');
-var loginRoutes= require('./routes/login');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 //busqueda
-var busquedaRoutes= require('./routes/busqueda');
+var busquedaRoutes = require('./routes/busqueda');
 //subir archivos
-var uploadRoutes= require('./routes/upload');
+var uploadRoutes = require('./routes/upload');
 
-var imagenesRoutes= require('./routes/imagenes');
+var imagenesRoutes = require('./routes/imagenes');
 
 //Conexion a la base de datos.
 
 //--------- Cambiar nombre a BPUS !!!!!!! ---------------
-mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',(err,res)=>{
-//--------- Cambiar nombre a BPUS !!!!!!! ---------------
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+    //--------- Cambiar nombre a BPUS !!!!!!! ---------------
 
-if(err) throw err;
+    if (err) throw err;
 
-console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 
 });
 
@@ -66,22 +69,25 @@ app.use('/uploads', serveIndex(__dirname + '/uploads'));*/
 //Rutas
 
 
-app.use('/sedeUniversitaria',sedeUniversitaria);
-app.use('/programaUniversitario',programaUniversitario);
-app.use('/facultad',facultad);
+app.use('/sedeUniversitaria', sedeUniversitaria);
+app.use('/programaUniversitario', programaUniversitario);
+app.use('/facultad', facultad);
+app.use('/facultad', anteProyecto);
+app.use('/facultad', proyecto);
+app.use('/facultad', articulo);
 
-app.use('/usuario',usuarioRoutes);
-app.use('/login',loginRoutes);
-app.use('/busqueda',busquedaRoutes);
-app.use('/upload',uploadRoutes);
-app.use('/img',imagenesRoutes);
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/busqueda', busquedaRoutes);
+app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
 
-app.use('/',appRoutes);
+app.use('/', appRoutes);
 
 //Escuchar peticiones
 
-app.listen(3000 ,()=>{
+app.listen(3000, () => {
 
-console.log('Express server puerto: \x1b[32m%s\x1b[0m', 'online');
+    console.log('Express server puerto: \x1b[32m%s\x1b[0m', 'online');
 
 });
---
+++ models/articulo.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var articuloChema = new Schema({ //Esquema con validaciones.
+
+    rutaArticulo: { type: String, unique: true, required: [true, 'La ruta del artículo es necesaria'] },
+    estado: { type: Boolean, default: false },
+
+});
+
+articuloChema.plugin(uniqueValidator, { message: '{PATH} debe ser unico' });
+
+// ruta estado
+module.exports = mongoose.model('articulo', articuloChema);
